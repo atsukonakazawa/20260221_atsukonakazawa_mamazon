a978041ae189ba45447d37986f63ee74736f3444
atsuko nakazawa <tsqe8qm1bmqztbxbjre9@docomo.ne.jp>
2026-06-24 11:27:42 +0900

    Cloudinary関連修正6

--- front/src/app/components/products/ProductSwiper.tsx
+++ front/src/app/components/products/ProductSwiper.tsx
@@ -19,21 +19,31 @@ export default function ProductSwiper({ images }: Props) {
     if (!images || images.length === 0) return null;
 
     return (
-    <Swiper
-        modules={[Pagination]}
-        slidesPerView={1}
-        spaceBetween={10}
-        pagination={{ clickable: true }}
-        className="w-full"
-        >
-        {images.map((img) => (
-            <SwiperSlide key={img.id}>
-            <img
-                src={`http://localhost/storage/${img.image_path}`}
-                className="w-full object-contain mx-auto"
-            />
-            </SwiperSlide>
-        ))}
+        <Swiper
+            modules={[Pagination]}
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{ clickable: true }}
+            className="w-full"
+            >
+            {images && images.length > 0 ? (
+                images.map((image) => (
+                    <SwiperSlide key={image.id}>
+                        <img
+                            src={image.image_path}
+                            alt="商品画像"
+                            className="w-full object-contain mx-auto"
+                        />
+                    </SwiperSlide>
+                ))
+            ) : (
+                <SwiperSlide>
+                    <img
+                        src="/no-image.png"
+                        alt="No image"
+                    />
+                </SwiperSlide>
+            )}
         </Swiper>
     );
 }
